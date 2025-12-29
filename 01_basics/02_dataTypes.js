ebb258b8da78e8f400db8b9422b6f6b568ebb695
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2025-12-29 17:25:37 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3 + 3) => we cannot use alert like this there is diff syntax
+// if we do this in browser it will happen but not for JS
+
+// tc39.es JS Standard which shows basic rules for particular topic
+
+let name = "Shreya" // string
+let age = 21 // int
+// let isLoggedIn = flase // boolean -> true or flase
+
+// number => 2 to power 53
+// bigint
+// null => standaloan value(empty) // it is not that value is not define, it means value is not assign yet
+// undefined => value not define
+// symbol (use in react) => unique
+
+
+
+// object 
+
+console.log(typeof null); // output - object
+console.log(typeof "shreya"); // output - string
