496897cf4842679c0a6af41dc778e2296942bbe4
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2025-12-30 16:43:46 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);// even tho there is abc type will come as number
+
+
+let score1 = null
+console.log(score1);
+
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true => 1; false => 0;
+
+let isLoggedIn = "shreya" // if null then false
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1- true
+// 0- false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
